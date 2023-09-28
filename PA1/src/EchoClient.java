d674b097797268bb046c24cb83c083dfe091c3c8
Jason Suh <jtrain@crc-dot1x-nat-10-239-174-251.bu.edu>
2023-09-28 16:31:15 -0400

    updates

--- PA1/src/EchoClient.java
+++ PA1/src/EchoClient.java
@@ -6,19 +6,19 @@ public class EchoClient {
     private PrintWriter out;
     private BufferedReader in;
 
-    public void startConnection(String ip, int port) {
+    public void startConnection(String ip, int port) throws UnknownHostException, IOException {
         clientSocket = new Socket(ip, port);
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     }
 
-    public String sendMessage(String msg) {
+    public String sendMessage(String msg) throws IOException {
         out.println(msg);
         String resp = in.readLine();
         return resp;
     }
 
-    public void stopConnection() {
+    public void stopConnection() throws IOException {
         in.close();
         out.close();
         clientSocket.close();
--- PA1/src/EchoServer.java
+++ PA1/src/EchoServer.java
@@ -7,27 +7,22 @@ public class EchoServer {
     private PrintWriter out;
     private BufferedReader in;
 
-    public void start(int port) {
+    public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         clientSocket = serverSocket.accept();
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String greeting = in.readLine();
-            if ("hello server".equals(greeting)) {
-                out.println("hello client");
-            }
-            else {
-                out.println("unrecognised greeting");
-            }
-    }
-
-    public void stop() {
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
-    }
-    public static void main(String[] args) {
+        
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+        if (".".equals(inputLine)) {
+            out.println("good bye");
+            break;
+         }
+         out.println(inputLine);
+        }
+    }   
+    public static void main(String[] args) throws IOException {
         EchoServer server=new EchoServer();
         server.start(6666);
     }
